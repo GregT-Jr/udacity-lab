b87f2ac97cd663601f0d3026b8af331b9be2d631
Gregory Taylor <gregory.jr@live.com>
2021-07-05 11:51:50 -0400

    Update per review

--- designs.js
+++ designs.js
@@ -1,33 +1,36 @@
 // Select color input
 const gridElement = document.querySelector('#pixelCanvas');
-gridElement.addEventListener('click', function(event) {  
-    var cell = event.target;
-    if(cell.tagName.toLowerCase()=="td"){
-     const color = document.querySelector('#colorPicker').value;  
-     event.target.style['background-color'] = color;
-   }
+gridElement.addEventListener('click', function(event) {
+    let cell = event.target;
+    if (cell.tagName.toLowerCase() == "td") {
+        const color = document.querySelector('#colorPicker').value;
+        event.target.style['background-color'] = color;
+    }
 });
 
 // Select size input
 document.addEventListener('submit', function(event) {
     event.preventDefault(); // prevent page reset
-    makeGrid();
+    makeGrid(); //call make grid fucntion when size is submitted
 });
 
 function makeGrid() {
     const height = document.querySelector('#inputHeight').value;
     const width = document.querySelector('#inputWidth').value;
+    
+    //resets grid on submit
     while (gridElement.firstChild) {
         gridElement.removeChild(gridElement.firstChild);
     }
 
-      let inputWidth = "";
-      for (let i = 0; i < width; i++) {
+    //take height and width values to create grid
+    let inputWidth = "";
+    for (let i = 0; i < width; i++) {
         inputWidth += '<td></td>';
     }
-    
-      const HTMLToAdd = '<tr>' + inputWidth + '</tr>';
-      for (let j = 0; j < height; j++) {
+
+    const HTMLToAdd = '<tr>' + inputWidth + '</tr>';
+    for (let j = 0; j < height; j++) {
         gridElement.insertAdjacentHTML('afterbegin', HTMLToAdd);
     }
 };
